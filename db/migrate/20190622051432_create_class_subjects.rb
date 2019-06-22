c1dca8ce2b9db4fa71881aa121f95b4500c64f00
rafaelcnagy <rafaelcnagy@hotmail.com>
2019-06-22 02:51:08 -0300

    Disciplinas adicionadas

--- db/migrate/20190622051432_create_class_subjects.rb
+++ db/migrate/20190622051432_create_class_subjects.rb
@@ -2,10 +2,10 @@ class CreateClassSubjects < ActiveRecord::Migration[5.2]
   def change
     create_table :class_subjects do |t|
       t.string :number
-      t.string :hour_begin
+      t.string :hour_begins
       t.string :hour_ends
 	  t.belongs_to :subject
-	  
+
       t.timestamps
     end
   end
